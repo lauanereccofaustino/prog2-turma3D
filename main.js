c85b10db21cccbfce961765d962f5c1bc0ffb967
Lauane Recco Faustino -3D <112412521+lauanereccofaustino@users.noreply.github.com>
2024-06-18 19:25:58 -0300

    bgfiyuh

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
+<script src="main.js"></script>
+const botoes = document.querySelectorAll(".botao");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+    }
 }
-
-function atualizaCronometro() {
-
-    for (let i = 0; i < contadores.length; i++) {
-        contadores[i].textContent = calculaTempo(tempos[i]);
-    }
-    @@ -52, 4 + 53, 4 @@function comecaCronometro() {
-        setInterval(atualizaCronometro, 1000);
-    }
-
-    comecaCronometro();
-//comecaCronometro();
