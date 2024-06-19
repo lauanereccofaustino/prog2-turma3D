20f4020b3e64f5e1240ce693600e0e9399c25a82
Lauane Recco Faustino -3D <112412521+lauanereccofaustino@users.noreply.github.com>
2024-06-19 19:44:02 -0300

    khggfguj

--- main.js
+++ main.js
@@ -10,8 +10,10 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
+
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
@@ -27,30 +29,29 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0) {
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
         return [dias, horas, minutos, segundos];
     } else {
-        return "Prazo Finalizado";
         return [0, 0, 0, 0];
     }
 }
 
 function atualizaCronometro() {
-
     document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
     document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
     document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
     document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
 
     for (let i = 0; i < contadores.length; i++) {
-        contadores[i].textContent = calculaTempo(tempos[i]);
         // contadores[i].textContent = calculaTempo(tempos[i]);   
+        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
     }
 }
 
-@@ -53, 4 + 57, 4 @@function comecaCronometro() {
+function comecaCronometro() {
+    atualizaCronometro();
     setInterval(atualizaCronometro, 1000);
 }
-
-//comecaCronometro();
 comecaCronometro();
