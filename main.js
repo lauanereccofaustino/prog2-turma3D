04b4d599efca0e1793f0ff2302bcd9be844b890c
Lauane Recco Faustino -3D <112412521+lauanereccofaustino@users.noreply.github.com>
2024-06-19 17:27:36 -0300

    gdytgiu

--- main.js
+++ main.js
@@ -11,3 +11,7 @@ for (let i = 0; i < botoes.length; i++) {
         botoes[i].classList.add("ativo");
     }
 }
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+
