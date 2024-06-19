dd84d53c8a5118a506d1e3771b3c666596092aa6
Lauane Recco Faustino -3D <112412521+lauanereccofaustino@users.noreply.github.com>
2024-06-19 17:36:36 -0300

    ghdrsthfghy

--- main.js
+++ main.js
@@ -20,3 +20,27 @@ contadores[1].textContent = calculaTempo(tempoObjetivo2);
 contadores[2].textContent = calculaTempo(tempoObjetivo3);
 contadores[3].textContent = calculaTempo(tempoObjetivo4);
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+contadores[0].textContent = calculaTempo(tempoObjetivo1); //Antes
+
+contadores[0].textContent = calculaTempo(tempos[0]); // Depois
+// Para cada objetivo na lista de contadores
+for (let i = 0; i < contadores.length; i++) {
+    //Calcular o tempo usando a função e associá-lo ao objetivo
+    contadores[i].textContent = calculaTempo(tempos[i]);
+    function atualizaCronometro() {
+        for (let i = 0; i < contadores.length; i++) {
+            contadores[i].textContent = calculaTempo(tempos[i]);
+        }
+    }
+
+    function comecaCronometro() {
+        atualizaCronometro();
+        setInterval(atualizaCronometro, 1000);
+    }
+
+    comecaCronometro();
+    if (tempoFinal > 0) {
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return "Prazo Finalizado";
+    }
