e7a4fb657d7da89b768beb02c12e4ef62036d674
Lauane Recco Faustino -3D <112412521+lauanereccofaustino@users.noreply.github.com>
2024-06-19 19:40:19 -0300

    hfggju

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
 const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
@@ -26,21 +26,31 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    if (tempoFinal > 0){
+    if (tempoFinal > 0) {
         return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+        return [dias, horas, minutos, segundos];
     } else {
         return "Prazo Finalizado";
+        return [0, 0, 0, 0];
     }
 }
 
-function atualizaCronometro(){
+function atualizaCronometro() {
 
-    for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);   
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i]);
+        // contadores[i].textContent = calculaTempo(tempos[i]);   
     }
-@@ -52,4 +53,4 @@ function comecaCronometro(){
-    setInterval(atualizaCronometro,1000);
 }
 
-comecaCronometro();
+@@ -53, 4 + 57, 4 @@function comecaCronometro() {
+    setInterval(atualizaCronometro, 1000);
+}
+
 //comecaCronometro();
+comecaCronometro();
